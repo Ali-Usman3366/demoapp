7dda35d703176f8614ac1e34e20fd4f295e0aea0
Ali-Usman3366 <104265108+Ali-Usman3366@users.noreply.github.com>
2022-09-22 22:21:23 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,8 @@ const Navbar=()=>{
       <div className="container-fluid">
             <NavLink className="navbar-brand" to="/">DREAM MOVIES</NavLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span></button>
+      <span class="navbar-toggler-icon"></span>
+    </button>
           
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mx-auto">
